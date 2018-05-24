8af21794a8caeb1ea1a78d227764607cee1c2d89
Luke <lukelovemoney@gmail.com>
2018-05-24 17:59:51 +0800

    添加调速button

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 !function () {
-    function writeCode(prefix, code, fn) {
-        let container = document.querySelector('#code')
-        let styleTag = document.querySelector('#styleTag')
-        let n = 0
-        let id = setInterval(() => {
-            n += 1
-            container.innerHTML = code.substring(0, n)
-            styleTag.innerHTML = code.substring(0, n)
-            container.scrollTop = container.scrollHeight
-            if(n >= code.length){
-                window.clearInterval(id)
-                fn&&fn.call()
-            }
-        }, 50)
+  var duration = 50
+
+  $('.actions').on('click', 'button', function (e) {
+    let $button = $(e.currentTarget) //this, button
+    let speed = $button.attr('data-speed')
+    $button.addClass('active')
+      .siblings('.active').removeClass('active')
+    switch (speed) {
+      case 'slow':
+        duration = 100
+        break
+      case 'normal':
+        duration = 50
+        break
+      case 'fast':
+        duration = 10
+        break
     }
-    let code = `
+  })
+  function writeCode(prefix, code, fn) {
+    let container = document.querySelector('#code')
+    let styleTag = document.querySelector('#styleTag')
+    let n = 0
+    setTimeout(function run() {
+      n += 1
+      container.innerHTML = code.substring(0, n)
+      styleTag.innerHTML = code.substring(0, n)
+      container.scrollTop = container.scrollHeight
+      if (n < code.length) {
+        setTimeout(run, duration)
+      } else {
+        fn && fn.call()
+      }
+    }, duration)
+  }
+  let code = `
 /*
  *我们来用css画一只皮卡丘吧
  */    
@@ -179,5 +199,8 @@
  *画好了，一只可爱的皮卡丘送给你~
  */
     `
-    writeCode('', code)
+  writeCode('', code)
+
+
+
 }.call()
